a2568f261cd374f0edb363591202d009929e5dbc
Ashutosh Kumar <ashutoshkr5115@gmail.com>
2025-06-03 12:06:34 +0530

    initial commit

---
+++ Accountcontroller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Accountcontroller extends Controller
+{
+    public function registration(){
+return view('front.account.registration');
+    }
+
+     public function login(){
+        
+    }
+}
